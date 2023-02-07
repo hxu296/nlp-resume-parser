0f8e4bfbb6f82b3c0df4d62307c930f2b7062715
baardl <bard.lind@gmail.com>
2023-02-07 14:40:08 +0100

    Create uploads, if not existing

--- build.sh
+++ build.sh
@@ -3,5 +3,6 @@
 # Build Assumptions:
 # Python3 and pip3 are installed properly
 # apt install build-essential libpoppler-cpp-dev pkg-config python3-dev
+mkdir -p application/uploads
 pip3 install -r build/requirements.txt
 echo "$0 finished running"
