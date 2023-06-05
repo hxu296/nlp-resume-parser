8ec125e671c02a363deb5d66592c1715c7a2cced
Arundas TC <arundas.tc@gmail.com>
2023-06-05 19:29:02 +0800

    Change port, conflicts with AirPlay Receiver in macOS

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
 export RESUME_PARSER_HOST=0.0.0.0
-export RESUME_PARSER_PORT=5000
+export RESUME_PARSER_PORT=5001
 
 cd application
 echo Parser Running at $RESUME_PARSER_HOST:$RESUME_PARSER_PORT
