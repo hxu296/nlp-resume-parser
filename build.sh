820caab650685e0b91a327f0bae72e81dfdb80c0
Arundas TC <arundas.tc@gmail.com>
2023-06-06 13:18:40 +0800

    Create the logs directory if missing

--- build.sh
+++ build.sh
@@ -3,6 +3,7 @@
 # Build Assumptions:
 # Python3 and pip3 are installed properly
 # apt install build-essential libpoppler-cpp-dev pkg-config python3-dev
+mkdir -p application/logs
 mkdir -p application/uploads
 pip3 install -r build/requirements.txt
 echo "$0 finished running"
